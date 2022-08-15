e602d21ccb687ca20a9ec4ac63d662a0e1c5e7cb
hadi <hadipsy27@gmail.com>
2022-08-15 11:28:54 +0700

    First Commit

---
+++ src/main/java/com/springboot/finalexamp/FinalExampApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.finalexamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/finalexamp/FinalExampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.finalexamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
